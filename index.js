7424bba394b410f8f79898dabb4130017559354c
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-22 14:36:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    string = this.sanitize(string);
+    const words = string.split(' ');
+    const newString = [];
+    words.forEach(word => {
+      if (exceptions.includes(word)) {
+        newString.push(word);
+      } else {
+        newString.push(this.capitalize(word));
+      }
+    });
+    newString[0] = this.capitalize(newString[0]);
+    return newString.join(' ');
+  }
 }
