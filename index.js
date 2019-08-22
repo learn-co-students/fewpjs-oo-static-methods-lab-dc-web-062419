bff356de713e30d5157feeb02df7a0560ed19e1c
lesliesage <leslie.sage@gmail.com>
2019-08-22 13:56:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str.slice(0,1).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  
+  static titleize(str){
+    const wordArray = str.split(" ");
+    const newWordArray = [];
+    let word;
+    for (let i = 0; i < wordArray.length; i++) {
+        word = this.sanitize(wordArray[i]);
+        if (["the","a","an","but","of","and","for","at","by","from"].includes(word)){
+            newWordArray.push(word)
+        } else {
+            newWordArray.push(this.capitalize(word))
+        }
+    };
+    return this.capitalize(newWordArray.join(" "));
+  }
 }
