65cd812f382036e8b9b40e359a38d00d35b77536
Trevahr Robertson <Trevahr@gmail.com>
2019-08-22 22:04:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+
+
+
+  static capitalize(string){
+
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+
+  static titleize(string){
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newArr = []
+      let newString = string.split(' ')
+    newString.forEach(elem => {
+      
+        if(arr.includes(elem)) {
+          newArr.push(elem) 
+        }
+        else{
+          newArr.push(Formatter.capitalize(elem))
+        }
+      
+    })
+    newArr[0] = Formatter.capitalize(newArr[0])
+    newArr = newArr.join(" ")
+    return newArr
+  }
+  
+
 }
+
+Formatter.titleize("This string here should be capitalized")
